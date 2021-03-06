a9e0ac1a43e5ed7490ea72d571d9e05072cb5fc2
romabash <romabash2@gmail.com>
2021-03-07 00:10:36 +0400

    add 1.2.4. VALIDATOR *

--- Task 1/Task 1.2/task1.2/task1.2/Program.cs
+++ Task 1/Task 1.2/task1.2/task1.2/Program.cs
@@ -7,7 +7,7 @@ namespace task1._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( DOUBLER("написать программу, которая", "описание"));
+            
         }
 
         static float Averages(string str)
@@ -39,5 +39,32 @@ namespace task1._2
             return newStr.ToString();
         }
 
+        static int LOWERCASE(string str)
+        {
+            int lower = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]==' ' && char.IsLower(str[i+1]))
+                {
+                    lower++;
+                }
+            }
+            return lower;
+        }
+        static string VALIDATOR(string str)
+        {
+            StringBuilder newStr =new StringBuilder( str);
+            newStr[0] = char.ToUpper(newStr[0]);
+            for (int i = 0; i < newStr.Length-2; i++)
+            {
+                
+                if ((newStr[i] =='.' || newStr[i] == '?' || newStr[i] == '!') && newStr[i+1] == ' ')
+                {
+                        newStr[i + 2] = char.ToUpper(newStr[i + 2]);
+                }
+            }
+            return newStr.ToString();
+
+        }
     }
 }
