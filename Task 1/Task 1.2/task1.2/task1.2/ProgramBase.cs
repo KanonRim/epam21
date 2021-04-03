af1ad7ac0534633c9d678bbe13f68b7d480a2cb9
romabash <romabash2@gmail.com>
2021-04-03 17:57:13 +0400

    Task 3.2.1

---
+++ Task 1/Task 1.2/task1.2/task1.2/ProgramBase.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Text;
+
+namespace task1._2
+{
+    public  class ProgramBase
+    {
+        public static string VALIDATOR(string str)
+        {
+            StringBuilder newStr = new StringBuilder(str);
+            newStr[0] = char.ToUpper(newStr[0]);
+            for (int i = 0; i < newStr.Length - 2; i++)
+            {
+
+                if ((newStr[i] == '.' || newStr[i] == '?' || newStr[i] == '!') && newStr[i + 1] == ' ')
+                {
+                    newStr[i + 2] = char.ToUpper(newStr[i + 2]);
+                }
+            }
+            return newStr.ToString();
+
+        }
+    }
+}
---
+++ Task 3/Task3.2.1/Task3.2.1/ConsoleHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._1
+{
+    public class ConsoleHelper
+    {
+        static public int IntReadParse(string Mess,int min,int max)
+        {
+            int data;
+            bool parse;
+            bool range;
+            do
+            {
+                Console.WriteLine(Mess);
+                parse = int.TryParse(Console.ReadLine(), out data);
+                range = data>=min && data<=max;
+
+            } while  (!parse || !range);
+            return data;
+        }
+
+        static public bool YesOrNo(string question)
+        {
+            Console.WriteLine(question);
+            ConsoleKey key = Console.ReadKey().Key;
+     
+            if ( key== ConsoleKey.Enter || key == ConsoleKey.Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }       
+        }
+    }
+}
