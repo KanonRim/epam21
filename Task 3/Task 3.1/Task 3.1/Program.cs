0f10e026f5d385a483aadce76712ec9e935f7540
romabash <romabash2@gmail.com>
2021-04-03 13:21:32 +0400

    Create Task 3.1

---
+++ Task 3/Task 3.1/Task 3.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Task_2._1;
+
+namespace Task_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = ConsoleHelper.IntReadParse("Введите N", 1, int.MaxValue);
+            MyClosedCollection<char> person = new MyClosedCollection<char>();
+            for (int i = 0; i < n; i++)
+            {
+                person.Add(Convert.ToChar((i + 1).ToString()));
+            }
+
+            var ie = person.GetEnumerator();
+            while (person.Length>=2)
+            {
+                ie.MoveNext();
+                ie.MoveNext();
+                person.Remove( ie.Current);
+                Console.WriteLine(new string (person.Mas));
+
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
